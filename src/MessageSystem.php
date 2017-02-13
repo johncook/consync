e8cb1a540bc985f4e01f095c4ffda5a5e49f6c8a
John Cook <john@johncook.me.uk>
2017-02-13 08:09:25 +0000

    Added system initial system classes.

---
+++ src/MessageSystem.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Drupal\consync;
+
+class MessageSystem implements MessageSystemInterface {
+
+}
---
+++ src/MessageSystemInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Drupal\consync;
+
+interface MessageSystemInterface {
+
+}
